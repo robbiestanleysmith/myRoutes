d280063bdad40c584a6a9460fed783e849255387
Robbie Stanley-Smith <robbie.stanleysmith@gmail.com>
2023-03-07 13:12:11 +0000

    created users controller, added first_name, last_name, age and location to user model, set up cloudinary and seeded 5 users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/migrate/20230307105654_create_routes.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoutes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :routes do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
