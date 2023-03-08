7aa9b108b531f127f091a74c0dd92e1ba0bd515a
Neil Cairns <neilcairns@me.com>
2023-03-08 15:10:04 +0000

    added video to home page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,8 +15,8 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+    // this.#addMarkersToMap()
+    // this.#fitMapToMarkers()
     this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl }))
 
