f3daeebd472bc01c6bcfba693d1f9542ebe3e0f4
Niklas Bakula <niklasjbakula@gmail.com>
2023-03-10 15:28:53 +0000

    Converting place name to coordinates half finished

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "JSON"
+
 class DestinationsController < ApplicationController
   def index
     @destinations = Destination.all
@@ -17,10 +20,21 @@ class DestinationsController < ApplicationController
     if dest
      @destination = dest
     else
+      address = "#{params[:destination][:address]}".gsub(/\s/, '%20')
+      query_text = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{address}.json?access_token=#{ENV['MAPBOX_API_KEY']}"
+      api_response = URI.open(query_text).read
+      api_json = JSON.parse(api_response)
       @destination = Destination.new(destination_params)
+      @destination.longitude = api_json["features"][0]["geometry"]["coordinates"][0]
+      @destination.latitude = api_json["features"][0]["geometry"]["coordinates"][1]
+      @destination.title = api_json["features"][0]["text"]
+      @destination.address = api_json["features"][1]["place_name"]
+      @destination.city = api_json["features"][1]["context"][3]["text"]
+      raise
       @destination.user = current_user
       @destination.save
     end
+
     RouteDestination.create(route: @route, destination: @destination)
     redirect_to edit_route_path(@route)
   end
