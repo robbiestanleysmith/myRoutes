e57a8878c93bde0632c135f76f02f41199e883e5
Niklas Bakula <niklasjbakula@gmail.com>
2023-03-13 17:39:30 +0000

    Address, title etc. is added to db when route is edited

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -20,23 +20,13 @@ class DestinationsController < ApplicationController
     if dest
      @destination = dest
     else
-      address = "#{params[:destination][:address]}".gsub(/\s/, '%20')
-      query_text = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{address}.json?access_token=#{ENV['MAPBOX_API_KEY']}"
-      api_response = URI.open(query_text).read
-      api_json = JSON.parse(api_response)
       @destination = Destination.new(destination_params)
-      @destination.longitude = api_json["features"][0]["geometry"]["coordinates"][0]
-      @destination.latitude = api_json["features"][0]["geometry"]["coordinates"][1]
-      @destination.title = api_json["features"][0]["text"]
-      @destination.address = api_json["features"][1]["place_name"]
-      @destination.city = api_json["features"][1]["context"][3]["text"]
-      raise
       @destination.user = current_user
       @destination.save
     end
 
     RouteDestination.create(route: @route, destination: @destination)
-    redirect_to edit_route_path(@route)
+    redirect_to edit_route_path(@route), status: :unprocessable_entity
   end
 
   def edit
@@ -51,12 +41,12 @@ class DestinationsController < ApplicationController
   def destroy
     @destination = Destination.find(params[:id])
     @destination.destroy
-    redirect_back(fallback_location: routes_path)
+    redirect_back(fallback_location: routes_path, status: :unprocessable_entity)
   end
 
   private
 
   def destination_params
-    params.require(:destination).permit(:title, :longitude, :latitude, :address)
+    params.require(:destination).permit(:title, :longitude, :latitude, :address, :city)
   end
 end
