7b13c1c382ded524e6f89bee02a3bdf4fec7b734
Niklas Bakula <niklasjbakula@gmail.com>
2023-03-14 21:05:09 +0000

    Added redirect to google incl POI and location

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,13 +1,18 @@
 class RoutesController < ApplicationController
   def index
     @routes = Route.all
+
+    # After seeding: Create URLs for all routes
+    @routes.each do |route|
+      route_destinations_ordered = route.route_destinations.order(position: :asc).map { |route_destination| route_destination.destination }
+      update_google_redirect(route_destinations_ordered, route)
+    end
   end
 
   def show
     @route = Route.find(params[:id])
     @destination = Destination.new
-
-    route_destinations = @route.route_destinations.order(position: :asc).map { |route_destination| route_destination.destination }
+    @route_destinations_ordered = @route.route_destinations.order(position: :asc).map { |route_destination| route_destination.destination }
 
     @markers = @route.destinations.geocoded.map do |destination|
       {
@@ -42,7 +47,13 @@ class RoutesController < ApplicationController
   def edit
     @route = Route.find(params[:id])
     @destination = Destination.new
-    route_destinations = @route.route_destinations.order(position: :asc).map { |route_destination| route_destination.destination }
+    @route_destinations_ordered = @route.route_destinations.order(position: :asc).map { |route_destination| route_destination.destination }
+
+    # If the current route has more than 2 destinations, update the routes google maps link
+    if @route_destinations_ordered.length >= 2
+      update_google_redirect(@route_destinations_ordered, @route)
+    end
+
     @markers = @route.destinations.geocoded.map do |destination|
       {
         lat: destination.latitude,
@@ -54,6 +65,7 @@ class RoutesController < ApplicationController
 
   def update
     @route = Route.find(params[:id])
+
     @route.update(route_params)
   end
 
@@ -64,6 +76,57 @@ class RoutesController < ApplicationController
   end
 
   private
+
+  def update_google_redirect(route_destinations_ordered, route)
+
+    if route_destinations_ordered.length >= 2
+
+      if route_destinations_ordered.first.title == "Custom location"
+        origin = route_destinations_ordered.first.address.gsub(/\s/, "+")
+      else
+        origin = route_destinations_ordered.first.title.gsub(/\s/, "+")
+        origin << "%2C"
+        origin << route_destinations_ordered.first.city.gsub(/\s/, "+")
+      end
+
+      if route_destinations_ordered.last.title == "Custom location"
+        destination = route_destinations_ordered.last.address.gsub(/\s/, "+")
+      else
+        destination = route_destinations_ordered.last.title.gsub(/\s/, "+")
+        destination << "%2C"
+        destination << route_destinations_ordered.last.city.gsub(/\s/, "+")
+      end
+
+      url = "https://www.google.com/maps/dir/?api=1&origin=#{origin}&destination=#{destination}&travelmode=walking"
+
+      if route_destinations_ordered.length >= 3
+        if route_destinations_ordered[1].title == "Custom location"
+          waypoint = route_destinations_ordered[1].address.gsub(/\s/, "+")
+        else
+          waypoint = route_destinations_ordered[1].title.gsub(/\s/, "+")
+        end
+        url << "&waypoints=#{waypoint}"
+
+        if route_destinations_ordered.length >= 4
+
+          route_destinations_ordered.each_with_index do |destination, index|
+            if index >= 2 && index != (route_destinations_ordered.length - 1)
+              if destination.title == "Custom location"
+                waypoint = destination.address.gsub(/\s/, "+")
+              else
+                waypoint = destination.title.gsub(/\s/, "+")
+              end
+              url << "%7C#{waypoint}"
+            end
+          end
+        end
+      end
+      route.google_url = url
+      route.save
+    else
+      flash.alert = "At least two route destinations are needed"
+    end
+  end
 
   def route_params
     params.require(:route).permit(:title, :photo, :city)
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,27 +13,6 @@ puts "Seeding 1 user..."
 avatar1 = URI.open("https://res.cloudinary.com/dcuj8efm3/image/upload/v1678277535/rachel2_zsjgbi.png")
 user1.photo.attach(io: avatar1, filename: "av1.jpg", content_type: "image/png")
 
-# user2 = User.create!(email: "spongebob@gmail.com", password: "squarepants", first_name: "Sponge", last_name: "Bob", age: 22, location: "Bristol")
-# puts "Seeding 2/5 users..."
-# avatar2 = URI.open("https://res.cloudinary.com/dcuj8efm3/image/upload/v1677762866/spongebob_igaeww.jpg")
-# user2.photo.attach(io: avatar2, filename: "av2.jpg", content_type: "image/jpg")
-
-# user3 = User.create!(email: "rick@gmail.com", password: "pickle", first_name: "Rick", last_name: "Morty", age: 73, location: "High Wickham")
-# puts "Seeding 3/5 users..."
-# avatar3 = URI.open("https://res.cloudinary.com/dcuj8efm3/image/upload/v1677764458/rick2_vwm1wj.png")
-# user3.photo.attach(io: avatar3, filename: "av3.jpg", content_type: "image/png")
-
-# user4 = User.create!(email: "bonnie@gmail.com", password: "killercouple", first_name: "Bonnie", last_name: "Clyde", age: 45, location: "Tonbridge")
-# puts "Seeding 4/5 users..."
-# avatar4 = URI.open("https://res.cloudinary.com/dcuj8efm3/image/upload/v1677764458/bonnie2_qnhju9.png")
-# user4.photo.attach(io: avatar4, filename: "av4.jpg", content_type: "image/jpg")
-
-# user5 = User.create!(email: "brian@gmail.com", password: "butter", first_name: "Brian", last_name: "Butterfield", age: 58, location: "Hampshire")
-# puts "Seeding 5/5 users..."
-# avatar5 = URI.open("https://res.cloudinary.com/dcuj8efm3/image/upload/v1677764458/brian2_lz7zhw.jpg")
-# user5.photo.attach(io: avatar5, filename: "av5.jpg", content_type: "image/jpg")
-
-
 puts "Seeding routes and destinations..."
 
 
@@ -95,14 +74,5 @@ RouteDestination.create!(route: route3, destination: dest12, position: 3)
 dest13 = Destination.create!(latitude: 48.887691, longitude: 2.340607, title: "Sacré Coeur de Montmartre", user: user1, city: "Paris", address: "35 Rue du Chevalier de la Barre, 75018 Paris, France")
 RouteDestination.create!(route: route3, destination: dest13, position: 4)
 
-
-
-
-# users = User.all.select { |user| user != user5 }
-# 6.times {
-#   start_date = Faker::Date.between(from: '2022-09-23', to: '2023-09-25')
-#   end_date = start_date + rand(1..90)
-#   Booking.create(pickup_date: start_date, dropoff_date: end_date, user: users.sample, car: brians_cars.sample, status: "requested")
-# }
 
 puts "Database seeded!"
