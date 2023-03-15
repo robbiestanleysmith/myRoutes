bac797e91034567cf90db2c7b803ceaaf4da92a2
Niklas Bakula <niklasjbakula@gmail.com>
2023-03-15 17:05:43 +0000

    Fixed route page and add button

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -16,24 +16,36 @@ class DestinationsController < ApplicationController
 
   def create
     @route = Route.find(params[:route_id])
-    dest = Destination.find_by(address: params[:destination][:address])
-    if dest
-     @destination = dest
-    else
-      @destination = Destination.new(destination_params)
-      @destination.user = current_user
-      @destination.save
-    end
 
-    if @route.route_destinations.length.to_i == 0
-      index = 1
-    else
-      index = @route.route_destinations.last.position.to_i + 1
-    end
+    # Reload page if no input value was given
+    if params[:destination][:address] == ""
+      flash.alert = "Please enter a location in the search field"
+      redirect_to edit_route_path(@route), status: :unprocessable_entity
 
-    RouteDestination.create(route: @route, destination: @destination, position: index)
-    # redirect_to edit_route_path(@route), status: :unprocessable_entity
-    redirect_to edit_route_path(@route)
+    elsif @route.route_destinations.length >= 9
+      flash.alert = "You cannot add more than 9 destinations"
+      redirect_to edit_route_path(@route), status: :unprocessable_entity
+
+    else
+      dest = Destination.find_by(address: params[:destination][:address])
+      if dest
+      @destination = dest
+      else
+        @destination = Destination.new(destination_params)
+        @destination.user = current_user
+        @destination.save
+      end
+
+      if @route.route_destinations.length.to_i == 0
+        index = 1
+      else
+        index = @route.route_destinations.last.position.to_i + 1
+      end
+
+      RouteDestination.create(route: @route, destination: @destination, position: index)
+      # redirect_to edit_route_path(@route), status: :unprocessable_entity
+      redirect_to edit_route_path(@route), status: :unprocessable_entity
+    end
   end
 
   def edit
